d1f245fb5a77a5062b87de1b006488e97cd7f7e8
Mikeu Dev <rikiruswandi28@gmail.com>
2026-02-04 15:48:54 +0700

    refactor: memindahkan komponen tata letak global ke `+layout.svelte` root.

--- apps/portal/src/routes/(guest)/+layout.svelte
+++ apps/portal/src/routes/(guest)/+layout.svelte
@@ -1,20 +1,5 @@
 <script lang="ts">
-	import { page } from '$app/state';
-	import { locales, localizeHref } from '$lib/paraglide/runtime';
-	import '../layout.css';
-	import favicon from '$lib/assets/favicon.svg';
-
 	let { children } = $props();
 </script>
 
-<svelte:head><link rel="icon" href={favicon} /></svelte:head>
-
 {@render children()}
-<div style="display:none">
-	{#each locales as locale (locale)}
-		<!-- eslint-disable svelte/no-navigation-without-resolve -->
-		<a href={localizeHref(page.url.pathname, { locale })}>
-			{locale}
-		</a>
-	{/each}
-</div>
---
+++ apps/portal/src/routes/+layout.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	import './layout.css';
+	import favicon from '$lib/assets/favicon.svg';
+	import { locales, localizeHref } from '$lib/paraglide/runtime';
+	import { page } from '$app/state';
+
+	let { children } = $props();
+</script>
+
+<svelte:head>
+	<link rel="icon" href={favicon} />
+</svelte:head>
+
+{@render children()}
+
+<div style="display:none">
+	{#each locales as locale (locale)}
+		<!-- eslint-disable svelte/no-navigation-without-resolve -->
+		<a href={localizeHref(page.url.pathname, { locale })}>
+			{locale}
+		</a>
+	{/each}
+</div>
