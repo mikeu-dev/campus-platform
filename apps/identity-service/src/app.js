312c906f32226abd3a71d28fa87d929958127d92
Mikeu Dev <rikiruswandi28@gmail.com>
2026-02-04 10:56:30 +0700

    feat: menambahkan logging ke file menggunakan Morgan di beberapa layanan dan memperbarui .gitignore untuk mengabaikan file log tersebut.

--- apps/identity-service/src/app.js
+++ apps/identity-service/src/app.js
@@ -2,13 +2,19 @@ const express = require('express');
 const cors = require('cors');
 const helmet = require('helmet');
 const morgan = require('morgan');
+const fs = require('fs');
+const path = require('path');
 
 const app = express();
+
+// Create a write stream (in append mode)
+const accessLogStream = fs.createWriteStream(path.join(__dirname, '../server.log'), { flags: 'a' });
 
 // Middlewares
 app.use(helmet());
 app.use(cors());
-app.use(morgan('dev'));
+app.use(morgan('dev')); // Log to console
+app.use(morgan('combined', { stream: accessLogStream })); // Log to file
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
